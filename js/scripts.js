64ccf9b0d8b592e4f48c3d813e5d64a11b3a218c
HenryRivera01 <hariverao@udistrital.edu.co>
2025-03-18 19:29:05 -0500

    Aplicando funciones basicas de eventos en JS e identificando la diferencia entre load y DOMContentLoad

--- js/scripts.js
+++ js/scripts.js
@@ -13,16 +13,67 @@ heading.classList.add('nueva-clase'); // agregandole una nueva clase al h2
 //querySelectorAll Retorna de 0 hasta todos los elementos que coincidan
 
 const enlaces = document.querySelectorAll('.navegacion a') //Nos selecciona o retorna todos los elementos enlaces de padre navegacion
-console.log(enlaces[0]) //Retorna Nodelist (Array), asi que podemos seleccionar que index elegir
+//console.log(enlaces[0]) //Retorna Nodelist (Array), asi que podemos seleccionar que index elegir
 
 enlaces[0].textContent = 'nuevo texto para enlaces'
 enlaces[0].href = 'https://google.com' //Podemos modificar el enlace 
 enlaces [0].classList.add('nueva-clase'); //Creando nueva clase en enlace
-enlaces[0].classList.remove('navegacion__enlace') //Eliminando clase de enlace, no requiere punto como en el add
+//enlaces[0].classList.remove('navegacion__enlace') //Eliminando clase de enlace, no requiere punto como en el add
 
 
 //querySelectorById (Mas antigua)
 
 const heading2 = document.getElementById('heading');
-console.log(heading2)
 
+
+
+//Generar nuevo enlace con JS
+
+const nuevoEnlace = document.createElement('A')//Se recomiendo crear las etiquetas en mayusculas
+
+//Agregar el href
+
+nuevoEnlace.href = 'nuevo-enlaces.html';
+
+//Agregar el texto
+
+nuevoEnlace.textContent = 'Este es mi nuevo enlace';
+
+//Agregar la clase navegacion__enlace
+
+nuevoEnlace.classList.add('navegacion__enlace')
+
+//Agregarlo al documento
+
+const navegacion = document.querySelector('.navegacion')
+navegacion.appendChild(nuevoEnlace);//appendChild es como decir agrega
+// x elemento como hijo de la clase principal, en este caso agregamos el nuevo enlace a la barra 
+
+
+//Eventos en JS , desde deslizar hasta dar click
+
+console.log(1)
+
+window.addEventListener('load', imprimir)
+//load espera a que el JS y los archivos que dependen del HTML esten listos
+//Una vez que lo que se ponga en el addEventListener se cumpla, el codigo siguiente se ejecuta
+
+window.onload = function(){
+    console.log(3)
+}
+
+document.addEventListener('DOMContentLoaded', function(){ // DOMContentLoad espera solo a que se descargue
+//el HTML por eso imprime mas rapido que el load ya que ese si espera hasta el css
+    console.log(4)
+})
+
+
+console.log(5)
+
+function imprimir () {
+    console.log(2)
+}
+
+window.onscroll = function() {
+        console.log('You are scrolling')
+}
